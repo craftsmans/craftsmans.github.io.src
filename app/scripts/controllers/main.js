d3e378077bf2aee5a7c3f4ed5b9cec05c410a5df
Eduards Sizovs <EDUARDSI@EDUARDSs-MacBook-Pro-2.local>
2014-02-03 22:10:18 +0200

    first version

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('craftsmansApp').controller('MainCtrl', function ($scope) {
+
+	$scope.scroll = function(selector) {
+  		$('body').scrollTo(selector, 800);
+
+	};
+
+	$scope.scrollAndEmphasize = function(selector) {
+		$('body').scrollTo(selector, 800);
+		$('#cleancode').collapse('show');
+	}
+
+});
