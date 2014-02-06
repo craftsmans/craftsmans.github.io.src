8bb899f9dc52c4135948a33eb70e78f119ba15c0
Eduards Sizovs <EDUARDSI@EDUARDSs-MacBook-Pro-2.local>
2014-02-07 00:34:30 +0200

    removed unused images; fixed css

--- grunt/config/usemin.js
+++ grunt/config/usemin.js
@@ -3,6 +3,10 @@ module.exports = {
   html: ['<%= yeoman.dist %>/{,*/}*.html'],
   css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
   options: {
-    assetsDirs: ['<%= yeoman.dist %>']
+    assetsDirs: ['<%= yeoman.dist %>'],
+	patterns: {
+		css: [[/(?:src=|url\(\s*)['"]?\.\.\/([^'"\)\?]+)['"]?\s*\)?/gm, 'Replacing reference to macbook.png']]
+	}    
   }
 }
+
