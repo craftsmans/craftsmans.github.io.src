1bd98b37c4e0c4f0df5fb00ccbb09be766efbdd1
Eduards Sizovs <EDUARDSI@EDUARDSs-MacBook-Pro-2.local>
2014-02-07 00:44:03 +0200

    fixed comment in js

--- grunt/config/usemin.js
+++ grunt/config/usemin.js
@@ -5,7 +5,7 @@ module.exports = {
   options: {
     assetsDirs: ['<%= yeoman.dist %>'],
 	patterns: {
-		css: [[/(?:src=|url\(\s*)['"]?\.\.\/([^'"\)\?]+)['"]?\s*\)?/gm, 'Replacing reference to macbook.png']]
+		css: [[/(?:src=|url\(\s*)['"]?\.\.\/([^'"\)\?]+)['"]?\s*\)?/gm, 'Replacing references to relative images one level up in the file tree']]
 	}    
   }
 }
