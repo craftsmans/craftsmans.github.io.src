b3543af94e6f0c770504070e760a443ddc9cd138
Eduards Sizovs <EDUARDSI@EDUARDSs-MacBook-Pro-2.local>
2014-02-17 12:26:13 +0200

    added icons to copy script

--- grunt/config/copy.js
+++ grunt/config/copy.js
@@ -8,6 +8,7 @@ module.exports = {
       src: [
         '*.{ico,png,txt}',
         '.htaccess',
+        'CNAME',
         '*.html',
         'views/{,*/}*.html',
         'bower_components/**/*',
